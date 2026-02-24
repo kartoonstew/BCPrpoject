9b24d2033cc977adb33916ef447c7af541857e30
Ryan Stewart <ryanstewart@Ryans-MBP.localdomain>
2026-02-24 13:39:08 -0500

    added rich text formatting

--- BC.py
+++ BC.py
@@ -1,4 +1,10 @@
 import json
+from rich import print
+from rich.panel import Panel
+from rich.console import Console
+from rich.rule import Rule
+
+console = Console()
 
 try:
     with open("BC_Compendium.json", "r") as file:
@@ -6,33 +12,37 @@ try:
 except FileNotFoundError:
     print("Compendium file not found. Check the file path.")
     exit()
-        
+
+console.print(Panel("[bold red]Welcome to Black Company[/bold red]", style="white"),  justify="center")      
+
 def sub(info): #handles individual subclasses + morale, black banner, commissary as lists with their features
     for x in index:
         if info == x.lower():
-            print(f'{info.title()}:')
+            console.print(Rule(f'[bold white]{info.title()}:[/bold white]', style="white"))
             print(f'{index[x]["description"]}\n')
             for y in index[x]['features']:
-                print(f'{y.title()}:')
-                print(f'{index[x]["features"][y]["description"]:}\n')
+                console.print(Panel(f'[bold white]{y.title()}:[/bold white]', style="white"),  justify="left")
+                print(f'  {index[x]["features"][y]["description"]:}\n')
         for y in index[x]:
             if info == y.lower():
-                print(f'\n{info.title()}: {index[x][y]["description"]}\n')
+                console.print(Rule(f'[bold white]{info.title()}[/bold white]', style="white"))
+                console.print(Panel(f'{index[x][y]["description"]}', style="white"), justify="center")
+                #console.print(Panel(Rule(f'[bold white]\n{info.title()}[/bold white]\n\n{index[x][y]["description"]}\n', style="white")),  justify="left")
                 for feat in index[x][y]['features']:
-                    print(feat.title())
+                    console.print(Panel(f'[bold white]{feat.title()}:[/bold white]', style="white"),  justify="left")
                     print(f'Level: {index[x][y]["features"][feat]["level"]}')
-                    print(f'{index[x][y]["features"][feat]["description"]}\n')
+                    print(f'  {index[x][y]["features"][feat]["description"]}\n')
 
 def list_section(section): #handles lists of traits, quirks, subclasses (but no subclass info)
     for x in index[section]:
-        print(f'{x.title()}:')
+        console.print(Panel(f'[bold white]{x.title()}:[/bold white]', style="white"),  justify="left")
         print(f'{index[section][x]["description"]}\n')
 
 def single_info(single): #handles searching individual quirks and traits
     for x in index:
         if single in index[x]:
-            print(f'{single.title()}:')
-            print(f'{index[x][single]["description"]}')
+            console.print(Panel(f'[bold white]{single.title()}[/bold white]', style="white"),  justify="left")
+            console.print(f'{index[x][single]["description"]}')
                 
 while True:
     choice = input("Make a choice, or type quit: ").strip().lower()
