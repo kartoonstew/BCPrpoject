e95a6ef170c45050c2a4c1b58a3d2298ec0dae77
kartoonstew <rachthesystem@gmail.com>
2026-03-12 08:30:20 -0400

    Update BC.py

--- BC.py
+++ BC.py
@@ -47,7 +47,7 @@ def single_info(single): #handles searching individual quirks and traits
 while True:
     choice = input("Make a choice, or type quit: ").strip().lower()
     if choice == "quit":
-        print("Did you know that Smalley and Oscar used to make love during their time in the navy? Ok, bye.")
+        print("Come again sooooooon.")
         break
     elif choice in index["subclasses"]:
         sub(choice)
@@ -58,6 +58,6 @@ while True:
     elif choice in index["traits"] or choice in index["quirks"]:
         single_info(choice)
     else:
-        print("You, the user, absolutely fucked something up. This is not on me.")
+        print("Invalid choice, please try again.")
 
 
